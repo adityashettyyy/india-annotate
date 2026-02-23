32bf4d05bb23761b0b898e59e5c88ebf57da5806
adityashettyyy <adityaravishetty@gmail.com>
2026-02-23 23:34:00 +0530

    added SESSIONS_ROOT

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,9 @@ import os
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 
+SESSIONS_ROOT = PROJECT_ROOT / "sessions"
+SESSIONS_ROOT.mkdir(parents=True, exist_ok=True)
+
 DATASET_ROOT = PROJECT_ROOT / "dataset"
 IMAGES_ROOT = DATASET_ROOT / "images"
 ANNOTATIONS_ROOT = DATASET_ROOT / "annotations"
